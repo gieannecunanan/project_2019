3414d336f6c75687d247a5f8f835721b00a9d368
Gieanne Cunanan <gieannecunanan@mmc.school.nz>
2019-08-01 12:23:13 +1200

    Added 2 labels to indicate the scores and lives. Added 2 textbox to show the amount of score there is and lives there is left for the user.

--- project_2019/Form1.Designer.cs
+++ project_2019/Form1.Designer.cs
@@ -30,9 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pnlGame = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.TextBox();
+            this.txtLives = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // pnlGame
@@ -45,14 +49,14 @@
             this.pnlGame.Size = new System.Drawing.Size(369, 334);
             this.pnlGame.TabIndex = 1;
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.BackColor = System.Drawing.Color.MistyRose;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(146, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtName.BackColor = System.Drawing.Color.MistyRose;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Location = new System.Drawing.Point(146, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(130, 20);
+            this.txtName.TabIndex = 1;
             // 
             // label2
             // 
@@ -79,15 +83,60 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Cherry Bomb";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MistyRose;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Gloucester MT Extra Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(482, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cherries";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MistyRose;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Gloucester MT Extra Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(482, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lives";
+            // 
+            // lblScore
+            // 
+            this.lblScore.BackColor = System.Drawing.Color.MistyRose;
+            this.lblScore.Location = new System.Drawing.Point(482, 158);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(57, 20);
+            this.lblScore.TabIndex = 7;
+            // 
+            // txtLives
+            // 
+            this.txtLives.BackColor = System.Drawing.Color.MistyRose;
+            this.txtLives.Location = new System.Drawing.Point(482, 267);
+            this.txtLives.Name = "txtLives";
+            this.txtLives.Size = new System.Drawing.Size(57, 20);
+            this.txtLives.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(626, 477);
+            this.Controls.Add(this.txtLives);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.pnlGame);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -99,9 +148,13 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlGame;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lblScore;
+        private System.Windows.Forms.TextBox txtLives;
     }
 }
 
